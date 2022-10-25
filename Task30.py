459b2567705172a512b5515241521d1940ded1ae
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-10-25 16:54:32 +0300

    Task30 correction

--- Task30.py
+++ Task30.py
@@ -1,8 +1,15 @@
 #Есть два файла: в одном хранятся ФИО пользователей сайта, а в другом — данные об их хобби. Известно, что при хранении данных используется принцип: одна строка — один пользователь. Написать код, загружающий данные из обоих файлов и формирующий из них словарь: ключи — ФИО, значения — данные о хобби.
 
-keys = open('users.txt').read().split('\n')
-values = open('hobbies.txt').read().split('\n')
-dictionary = dict(zip(keys, values))
+keys = open('users.txt','r')
+keys_content = keys.read().split('\n')
+values = open('hobbies.txt','r')
+values_content = values.read().split('\n')
+dictionary = dict(zip(keys_content, values_content))
 
 import json
-open('users_hobbies.txt', 'w').write(json.dumps(dictionary, indent =4, separators=(" ", ',')))
+result = open('users_hobbies.txt','w')
+result_content = result.write(json.dumps(dictionary, indent =4, separators=(" ", ',')))
+
+keys.close()
+values.close()
+result.close()
